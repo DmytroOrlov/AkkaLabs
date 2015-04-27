b3358f1a5a6bcde4e6644c92257e34a166df69a9
Dmytro Orlov <orlov.dm@gmail.com>
2015-04-27 17:05:14 +0300

    add links

--- src/main/java/com/luxoft/akkalabs/day1/wikipedia/TweetLinksActor.java
+++ src/main/java/com/luxoft/akkalabs/day1/wikipedia/TweetLinksActor.java
@@ -2,6 +2,7 @@ package com.luxoft.akkalabs.day1.wikipedia;
 
 import akka.actor.ActorRef;
 import akka.actor.UntypedActor;
+import com.luxoft.akkalabs.clients.twitter.TweetObject;
 
 public class TweetLinksActor extends UntypedActor {
 
@@ -13,6 +14,8 @@ public class TweetLinksActor extends UntypedActor {
 
     @Override
     public void onReceive(Object message) throws Exception {
-        //..
+        if (message instanceof TweetObject)
+            for (String url : ((TweetObject) message).getUrls())
+                linksActor.tell(url, getSelf());
     }
 }
