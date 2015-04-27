b72d7cc7e192b7c711e1c12f625d49bc5c14277d
Dmytro Orlov <orlov.dm@gmail.com>
2015-04-27 14:50:07 +0300

    add onSuccess

--- src/main/java/com/luxoft/akkalabs/day1/futures/AppleVsGoogle.java
+++ src/main/java/com/luxoft/akkalabs/day1/futures/AppleVsGoogle.java
@@ -2,11 +2,16 @@ package com.luxoft.akkalabs.day1.futures;
 
 import akka.actor.ActorSystem;
 import akka.dispatch.Futures;
+import akka.dispatch.OnSuccess;
+import scala.Function1;
 import scala.concurrent.Await;
 import scala.concurrent.Future;
 import scala.concurrent.duration.Duration;
+import scala.runtime.BoxedUnit;
 
-import static java.util.concurrent.TimeUnit.SECONDS;
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 public class AppleVsGoogle {
 
@@ -16,21 +21,32 @@ public class AppleVsGoogle {
         final CollectTweets appleFunction = new CollectTweets(system, "Apple");
         final CollectTweets googleFunction = new CollectTweets(system, "Google");
 
-        final Future<FinalResult> appleFuture = Futures.future(appleFunction, system.dispatcher()).map(new ResultMapper(), system.dispatcher());
-        final Future<FinalResult> googleFuture = Futures.future(googleFunction, system.dispatcher()).map(new ResultMapper(), system.dispatcher());
+        final Future<FinalResult> appleFuture = Futures.future(appleFunction, system.dispatcher()).
+                map(new ResultMapper(), system.dispatcher());
+        final Future<FinalResult> googleFuture = Futures.future(googleFunction, system.dispatcher()).
+                map(new ResultMapper(), system.dispatcher());
 
-        final FinalResult appleResult = Await.result(appleFuture, Duration.create(60, SECONDS));
-        final FinalResult googleResult = Await.result(googleFuture, Duration.create(60, SECONDS));
+        final List<Future<FinalResult>> allFutures = Arrays.asList(appleFuture, googleFuture);
 
-        print(appleResult);
-        print(googleResult);
+        final Future<Iterable<FinalResult>> sequence = Futures.sequence(allFutures, system.dispatcher());
+        sequence.onSuccess(new OnSuccess<Iterable<FinalResult>>() {
+            @Override
+            public void onSuccess(Iterable<FinalResult> result) throws Throwable {
+                print(result);
+            }
+        }, system.dispatcher());
+
+        Await.result(sequence, Duration.create(60, TimeUnit.SECONDS));
+
         system.shutdown();
     }
 
-    private static void print(FinalResult res) {
-        System.out.println(res.getKeyword() + ":");
-        for (String lang : res.getLanguages().keySet()) {
-            System.out.println(lang + " " + res.getLanguages().get(lang));
+    private static void print(Iterable<FinalResult> results) {
+        for (FinalResult result : results) {
+            System.out.println(result.getKeyword() + ":");
+            for (String lang : result.getLanguages().keySet()) {
+                System.out.println(lang + " " + result.getLanguages().get(lang));
+            }
         }
     }
 }
