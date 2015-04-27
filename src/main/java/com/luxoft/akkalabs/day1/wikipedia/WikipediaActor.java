eb81d5372110bedeedc2bc90433feb3c298d3723
Dmytro Orlov <orlov.dm@gmail.com>
2015-04-27 17:18:24 +0300

    add get page

--- src/main/java/com/luxoft/akkalabs/day1/wikipedia/WikipediaActor.java
+++ src/main/java/com/luxoft/akkalabs/day1/wikipedia/WikipediaActor.java
@@ -1,11 +1,24 @@
 package com.luxoft.akkalabs.day1.wikipedia;
 
 import akka.actor.UntypedActor;
+import com.luxoft.akkalabs.clients.wikipedia.WikipediaClient;
+import com.luxoft.akkalabs.clients.wikipedia.WikipediaPage;
+
+import java.net.URL;
 
 public class WikipediaActor extends UntypedActor {
 
     @Override
     public void onReceive(Object message) throws Exception {
-        //...
+        if (message instanceof String) {
+            final URL url = new URL((String) message);
+            if (url.getHost().toLowerCase().endsWith(".wikipedia.org") && url.getPath().length() > 6) {
+                final String lang = url.getHost().substring(0, 2);
+                final String term = url.getPath().substring(6);
+                final WikipediaPage page = WikipediaClient.getPage(lang, term);
+                if (page != null)
+                    System.out.println(page.getTitle());
+            }
+        }
     }
 }
