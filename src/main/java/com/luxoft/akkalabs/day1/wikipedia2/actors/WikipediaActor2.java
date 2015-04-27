0b61ac2de1376bd232e4b44d16c271808c390c01
Dmytro Orlov <orlov.dm@gmail.com>
2015-04-27 17:35:57 +0300

    add actor2

--- src/main/java/com/luxoft/akkalabs/day1/wikipedia2/actors/WikipediaActor2.java
+++ src/main/java/com/luxoft/akkalabs/day1/wikipedia2/actors/WikipediaActor2.java
@@ -1,13 +1,24 @@
 package com.luxoft.akkalabs.day1.wikipedia2.actors;
 
+import akka.actor.ActorRef;
+import akka.actor.Props;
 import akka.actor.UntypedActor;
+import com.luxoft.akkalabs.day1.wikipedia.WikipediaActor;
 
 public class WikipediaActor2 extends UntypedActor {
 
+    private ActorRef connections;
+
     @Override
     public void onReceive(Object message) throws Exception {
-        if (message instanceof String) {
-            //...
-        }
+        if (message instanceof String)
+            for (String title : WikipediaActor.getTitle((String) message).asSet())
+                connections.tell(title, self());
+    }
+
+    @Override
+    public void preStart() throws Exception {
+        super.preStart();
+        connections = context().actorOf(Props.create(ConnectionsActor.class));
     }
 }
