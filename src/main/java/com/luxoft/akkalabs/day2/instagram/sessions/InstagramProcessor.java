4f209102c96fb837f5ffb0fb0a295713ea3227f0
Dmytro Orlov <orlov.dm@gmail.com>
2015-04-28 16:33:02 +0300

    add insta init

--- src/main/java/com/luxoft/akkalabs/day2/instagram/sessions/InstagramProcessor.java
+++ src/main/java/com/luxoft/akkalabs/day2/instagram/sessions/InstagramProcessor.java
@@ -2,11 +2,11 @@ package com.luxoft.akkalabs.day2.instagram.sessions;
 
 import akka.actor.ActorContext;
 import com.luxoft.akkalabs.day2.sessions.SessionProcessor;
-import java.io.IOException;
+
 import javax.websocket.Session;
+import java.io.IOException;
 
 public class InstagramProcessor implements SessionProcessor {
-
     private String sessionId;
     private ActorContext context;
     private Session session;
@@ -16,20 +16,20 @@ public class InstagramProcessor implements SessionProcessor {
         this.sessionId = sessionId;
         this.context = context;
         this.session = session;
-
     }
 
     @Override
     public void stopped() throws IOException {
-        //...
+        System.out.println("stop");
     }
 
     @Override
     public void incoming(String message) {
+        System.out.println("in");
     }
 
     @Override
     public void outgoing(Object message) throws IOException {
-        //...
+        System.out.println("out");
     }
 }
