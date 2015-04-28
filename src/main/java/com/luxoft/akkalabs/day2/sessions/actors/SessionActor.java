fcb2ca639cfe2e3af8a61fdfd546ec10f15eaefc
Dmytro Orlov <orlov.dm@gmail.com>
2015-04-28 11:19:19 +0300

    add hub

--- src/main/java/com/luxoft/akkalabs/day2/sessions/actors/SessionActor.java
+++ src/main/java/com/luxoft/akkalabs/day2/sessions/actors/SessionActor.java
@@ -1,19 +1,55 @@
 package com.luxoft.akkalabs.day2.sessions.actors;
 
 import akka.actor.UntypedActor;
+import com.luxoft.akkalabs.day2.sessions.SessionProcessor;
 import com.luxoft.akkalabs.day2.sessions.messages.Incoming;
 import com.luxoft.akkalabs.day2.sessions.messages.Outgoing;
 
+import javax.websocket.MessageHandler;
+import javax.websocket.Session;
+
 public class SessionActor extends UntypedActor {
+//    private final Session session;
+//    public SessionActor(Session session) {
+//        this.session = session;
+//    }
+
+    private final String sessionId;
+    private final Session session;
+    private final SessionProcessor processor;
+
+    public SessionActor(String sessionId, Session session, SessionProcessor processor) {
+        this.sessionId = sessionId;
+        this.session = session;
+        this.processor = processor;
+    }
 
     @Override
     public void onReceive(Object message) throws Exception {
         if (message instanceof Incoming) {
-            //...
+            processor.incoming(((Incoming) message).getMessage());
         } else if (message instanceof Outgoing) {
-            //...
+            processor.outgoing(((Outgoing) message).getMessage());
         } else {
-            //...
+            processor.outgoing(message);
         }
+    }
+
+    @Override
+    public void preStart() throws Exception {
+        super.preStart();
+        session.addMessageHandler(new MessageHandler.Whole<String>() {
+            @Override
+            public void onMessage(String msg) {
+                self().tell(new Incoming(msg), self());
+            }
+        });
+        processor.started(sessionId, context(), session);
+    }
+
+    @Override
+    public void postStop() throws Exception {
+        super.postStop();
+        processor.stopped();
     }
 }
--- src/main/java/com/luxoft/akkalabs/day2/sessions/actors/SessionsHubActor.java
+++ src/main/java/com/luxoft/akkalabs/day2/sessions/actors/SessionsHubActor.java
@@ -1,5 +1,6 @@
 package com.luxoft.akkalabs.day2.sessions.actors;
 
+import akka.actor.Props;
 import akka.actor.UntypedActor;
 import com.luxoft.akkalabs.day2.sessions.SessionProcessor;
 import com.luxoft.akkalabs.day2.sessions.messages.OutgoingBroadcast;
@@ -7,9 +8,13 @@ import com.luxoft.akkalabs.day2.sessions.messages.OutgoingToSession;
 import com.luxoft.akkalabs.day2.sessions.messages.RegisterSession;
 import com.luxoft.akkalabs.day2.sessions.messages.UnregisterSession;
 
+import javax.websocket.Session;
+
 public class SessionsHubActor extends UntypedActor {
 
     private final Class<? extends SessionProcessor> processorClass;
+
+//    private final Map<String, Session> sessionMap = new HashMap<>();
 
     public SessionsHubActor(Class<? extends SessionProcessor> processor) {
         this.processorClass = processor;
@@ -18,13 +23,21 @@ public class SessionsHubActor extends UntypedActor {
     @Override
     public void onReceive(Object message) throws Exception {
         if (message instanceof RegisterSession) {
-            //...
+            final String sessionId = ((RegisterSession) message).getSessionId();
+            final Session session = ((RegisterSession) message).getSession();
+//            sessionMap.put(sessionId, session);
+            context().actorOf(Props.create(SessionActor.class, sessionId, session, processorClass.newInstance()), sessionId);
         } else if (message instanceof UnregisterSession) {
-            //...
+            final String sessionId = ((UnregisterSession) message).getSessionId();
+//            sessionMap.remove(sessionId);
+            context().stop(getContext().getChild(sessionId));
         } else if (message instanceof OutgoingToSession) {
-            //...
+//            final Session session = sessionMap.get(((OutgoingToSession) message).getSessionId());
+            final String sessionId = ((OutgoingToSession) message).getSessionId();
+            getContext().getChild(sessionId).forward(message, context());
         } else if (message instanceof OutgoingBroadcast) {
-            //...
+//            for (Session session : sessionMap.values());
+            getContext().getChild("*").forward(message, context());
         }
     }
 }
