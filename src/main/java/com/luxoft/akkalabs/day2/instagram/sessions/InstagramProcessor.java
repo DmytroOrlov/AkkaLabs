0d67c1b1143d7578d38fc47d0ad06b8a98c4ec88
Dmytro Orlov <orlov.dm@gmail.com>
2015-04-28 16:44:23 +0300

    add proc

--- src/main/java/com/luxoft/akkalabs/day2/instagram/sessions/InstagramProcessor.java
+++ src/main/java/com/luxoft/akkalabs/day2/instagram/sessions/InstagramProcessor.java
@@ -1,7 +1,10 @@
 package com.luxoft.akkalabs.day2.instagram.sessions;
 
 import akka.actor.ActorContext;
+import com.luxoft.akkalabs.clients.twitter.TweetObject;
 import com.luxoft.akkalabs.day2.sessions.SessionProcessor;
+import com.luxoft.akkalabs.day2.topics.messages.SubscribeToTopic;
+import com.luxoft.akkalabs.day2.topics.messages.UnsubscribeFromTopic;
 
 import javax.websocket.Session;
 import java.io.IOException;
@@ -16,11 +19,13 @@ public class InstagramProcessor implements SessionProcessor {
         this.sessionId = sessionId;
         this.context = context;
         this.session = session;
+
+        context.system().actorSelection("/user/topics").tell(new SubscribeToTopic("instagram"), context.self());
     }
 
     @Override
     public void stopped() throws IOException {
-        System.out.println("stop");
+        context.system().actorSelection("/user/topics").tell(new UnsubscribeFromTopic("instagram"), context.self());
     }
 
     @Override
@@ -30,6 +35,9 @@ public class InstagramProcessor implements SessionProcessor {
 
     @Override
     public void outgoing(Object message) throws IOException {
-        System.out.println("out");
+        if (message instanceof TweetObject)
+            for (String url : ((TweetObject) message).getUrls())
+                if (url.startsWith("https://instagram.com/p/") && Math.random() < 0.1)
+                    context.self().tell(url, context.self());
     }
 }
