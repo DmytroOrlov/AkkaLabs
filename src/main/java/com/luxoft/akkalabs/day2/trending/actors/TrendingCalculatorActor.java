f7cf84438558642ccdfd269b42e835efb8d4c442
Dmytro Orlov <orlov.dm@gmail.com>
2015-04-28 15:04:33 +0300

    add words

--- src/main/java/com/luxoft/akkalabs/day2/trending/actors/TrendingCalculatorActor.java
+++ src/main/java/com/luxoft/akkalabs/day2/trending/actors/TrendingCalculatorActor.java
@@ -1,29 +1,50 @@
 package com.luxoft.akkalabs.day2.trending.actors;
 
 import akka.actor.UntypedActor;
+import com.google.common.base.Predicate;
+import com.google.common.collect.FluentIterable;
 import com.luxoft.akkalabs.clients.twitter.TweetObject;
 import com.luxoft.akkalabs.day2.trending.messages.UpvoteTrending;
+import scala.concurrent.duration.FiniteDuration;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static java.util.concurrent.TimeUnit.SECONDS;
 
 public class TrendingCalculatorActor extends UntypedActor {
+    private static final Object PING = new Object();
 
-    private final Object PING = new Object();
-    
+    private final Map<String, Integer> words = new HashMap<>();
+
     @Override
     public void preStart() throws Exception {
+        final FiniteDuration oneSecond = FiniteDuration.create(1, SECONDS);
+        context().system().scheduler().schedule(oneSecond, oneSecond,
+                self(), PING,
+                context().dispatcher(), self());
     }
 
     @Override
     public void postStop() throws Exception {
         super.postStop();
     }
-    
-    
+
 
     @Override
     public void onReceive(Object message) throws Exception {
         if (message == PING) {
         } else if (message instanceof TweetObject) {
-        
+            final FluentIterable<String> ws = FluentIterable.of(((TweetObject) message).getText().split(" ")).filter(new Predicate<String>() {
+                @Override
+                public boolean apply(String input) {
+                    return input.length() > 3 && !input.startsWith("http://") && !input.startsWith("https://");
+                }
+            });
+            for (String w : ws) {
+                Integer i = words.get(w);
+                words.put(w, (i != null ? i : 0) + 1);
+            }
         } else if (message instanceof UpvoteTrending) {
 
         }
