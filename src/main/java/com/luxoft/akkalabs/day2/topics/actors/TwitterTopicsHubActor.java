06d08ff1c64660992133b246947228ebd03014f6
Dmytro Orlov <orlov.dm@gmail.com>
2015-04-28 12:31:38 +0300

    add twitter hub

--- src/main/java/com/luxoft/akkalabs/day2/topics/actors/TwitterTopicsHubActor.java
+++ src/main/java/com/luxoft/akkalabs/day2/topics/actors/TwitterTopicsHubActor.java
@@ -1,7 +1,12 @@
 package com.luxoft.akkalabs.day2.topics.actors;
 
-import com.luxoft.akkalabs.day2.topics.messages.*;
+import akka.actor.ActorRef;
+import akka.actor.Props;
 import akka.actor.UntypedActor;
+import com.luxoft.akkalabs.day2.topics.messages.StopTopic;
+import com.luxoft.akkalabs.day2.topics.messages.SubscribeToTopic;
+import com.luxoft.akkalabs.day2.topics.messages.TopicIsEmpty;
+import com.luxoft.akkalabs.day2.topics.messages.UnsubscribeFromTopic;
 
 public class TwitterTopicsHubActor extends UntypedActor {
 
@@ -14,11 +19,21 @@ public class TwitterTopicsHubActor extends UntypedActor {
     @Override
     public void onReceive(Object message) throws Exception {
         if (message instanceof SubscribeToTopic) {
-
+            final String keyword = ((SubscribeToTopic) message).getKeyword();
+            ActorRef child = getContext().getChild(keyword);
+            if (child == null)
+                child = context().actorOf(Props.create(TwitterTopicActor.class), keyword);
+            child.forward(message, context());
         } else if (message instanceof UnsubscribeFromTopic) {
-
+            final String keyword = ((UnsubscribeFromTopic) message).getKeyword();
+            final ActorRef child = getContext().getChild(keyword);
+            if (child != null)
+                child.forward(message, context());
         } else if (message instanceof TopicIsEmpty) {
-            
+            final String keyword = ((TopicIsEmpty) message).getKeyword();
+            final ActorRef child = getContext().getChild(keyword);
+            if (child != null)
+                child.tell(StopTopic.getInstance(), self());
         }
     }
 }
