06eef255c7b99c83d021c5541929af721ce78cec
Dmytro Orlov <orlov.dm@gmail.com>
2015-04-28 12:40:16 +0300

    add sub/unsub

--- src/main/java/com/luxoft/akkalabs/day2/topics/actors/TwitterTopicActor.java
+++ src/main/java/com/luxoft/akkalabs/day2/topics/actors/TwitterTopicActor.java
@@ -1,15 +1,21 @@
 package com.luxoft.akkalabs.day2.topics.actors;
 
+import akka.actor.ActorRef;
 import akka.actor.UntypedActor;
 import com.luxoft.akkalabs.clients.twitter.QueueTwitterClient;
 import com.luxoft.akkalabs.clients.twitter.TweetObject;
 import com.luxoft.akkalabs.clients.twitter.TwitterClients;
 import com.luxoft.akkalabs.day2.topics.messages.StopTopic;
 import com.luxoft.akkalabs.day2.topics.messages.SubscribeToTopic;
+import com.luxoft.akkalabs.day2.topics.messages.TopicIsEmpty;
 import com.luxoft.akkalabs.day2.topics.messages.UnsubscribeFromTopic;
+
+import java.util.HashSet;
+import java.util.Set;
 
 public class TwitterTopicActor extends UntypedActor {
 
+    private final Set<ActorRef> subscribers = new HashSet<>();
     private final String keyword;
 
     private QueueTwitterClient twitterClient;
@@ -23,9 +29,11 @@ public class TwitterTopicActor extends UntypedActor {
         if (message instanceof TweetObject) {
 
         } else if (message instanceof SubscribeToTopic) {
-
+            subscribers.add(sender());
         } else if (message instanceof UnsubscribeFromTopic) {
-
+            subscribers.remove(sender());
+            if (subscribers.isEmpty())
+                getContext().parent().tell(new TopicIsEmpty(keyword), self());
         } else if (message instanceof StopTopic) {
 
         }
